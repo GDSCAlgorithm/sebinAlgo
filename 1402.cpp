93d62b72dd805440447446200fb3c9fafc3993ec
나세빈 <94737768+sebbbin@users.noreply.github.com>
2024-01-20 19:52:21 +0900

    240120

---
+++ 1402.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+ios_base::sync_with_stdio(false); 
+cin.tie(nullptr); cout.tie(nullptr);
+int t;
+cin >> t;
+int a, b;
+while(t--){
+cin >> a >> b;
+cout << "yes"<<"\n";
+}
+
+return 0;
+}
